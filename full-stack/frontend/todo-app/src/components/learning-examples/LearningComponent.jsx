ade1062a2622fc1cdd4a1228636af81073f13ad0
Jaeework <jaeework@gmail.com>
2024-03-11 07:08:24 +0900

    섹션9-5. 10. 11-6 React 기초
    React 기초 및 라우팅

---
+++ full-stack/frontend/todo-app/src/components/learning-examples/LearningComponent.jsx
@@ -0,0 +1,23 @@
+//import FirstComponent, { FourthComponent } from './FirstComponent';
+import LearningJavaScript from './LearningJavaScript';
+//import SecondComponent from './SecondComponent';
+//import ThirdComponent from './ThirdComponent';
+
+// export default function LearningComponent() {
+//     return (
+//         <>
+//             <FirstComponent />
+//             <SecondComponent />
+//             <ThirdComponent />
+//             <FourthComponent />
+//         </>
+//     );
+// }
+
+export default function LearningComponent() {
+    return (
+        <>
+            <LearningJavaScript />
+        </>
+    );
+}
