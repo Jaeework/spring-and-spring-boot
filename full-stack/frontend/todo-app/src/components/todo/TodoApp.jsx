831c5ed9ab4ce76c45b97cdc344619e71829a4d6
Jaeework <jaeework@gmail.com>
2024-03-18 13:52:26 +0900

    섹션11-7 ~ 8 필요한 컴포넌트 작성으로 Todo 애플리케이션 기본 화면 틀 구성
    인증 컨텍스트를 붙이기 전 Todo 애플리케이션을 구성하는 화면 컴포넌트들의 기본 틀 작성

--- full-stack/frontend/todo-app/src/components/todo/TodoApp.jsx
+++ full-stack/frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -1,80 +1,28 @@
-import { useState } from 'react';
-import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './TodoApp.css'
+import LoginComponent from './LoginComponent';
+import LogoutComponent from './LogoutComponent';
+import HeaderComponent from './HeaderComponent';
+import ListTodosComponent from './ListTodosComponent';
+import ErrorComponent from './ErrorComponent';
+import WelcomeComponent from './WelcomeComponent';
 
 export default function TodoApp() {
     return (
         <div className="TodoApp">
             <BrowserRouter>
+            <HeaderComponent />
+
                 <Routes>
-                    <Route path='/' element={<LoginComponent/>}></Route>
-                    <Route path='/login' element={<LoginComponent />}></Route>
-                    <Route path='/welcome' element={<WelcomeComponent />}></Route>
+                    <Route path='/' element={<LoginComponent/>} />
+                    <Route path='/login' element={<LoginComponent />} />
+                    <Route path='/welcome/:username' element={<WelcomeComponent />} />
+                    <Route path="/todos" element={<ListTodosComponent />} />
+                    <Route path="/logout" element={<LogoutComponent />} />
+
+                    <Route path='*' element={<ErrorComponent />} />
                 </Routes>
             </BrowserRouter>
-        </div>
-    );
-}
-
-function LoginComponent() {
-
-    const [username, setUsername] = useState('in28minutes');
-    const [password, setPassword] = useState('');
-    
-    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-    const [showErrorMessage, setShowErrorMessage] = useState(false);
-
-    const navigate = useNavigate();
-
-
-    function handleUsernameChange(event) {
-        setUsername(event.target.value);
-    }
-
-    function handlePasswordChange(event) {
-        setPassword(event.target.value);
-    }
-
-    function handleSubmit() {
-        if(username==='in28minutes' && password==='dummy') {
-            console.log('Succes');
-            setShowSuccessMessage(true);
-            setShowErrorMessage(false);
-
-            navigate('/welcome');
-        } else {
-            console.log('Failed');
-            setShowSuccessMessage(false);
-            setShowErrorMessage(true);
-        }
-    }
-
-    return (
-        <div className="Login">
-            {showSuccessMessage && <div className="successMessage">Authenticated Successful</div>}
-            {showErrorMessage && <div className="errorMessage">Authentication Filed. Please check your credentials.</div>}
-            <div className="LoginForm">
-                <div>
-                    <label>User Name:</label>
-                    <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
-                </div>
-                <div>
-                    <button type="button" name="login" onClick={handleSubmit}>login</button>
-                </div>
-            </div>
-        </div>
-    );
-}
-
-
-function WelcomeComponent() {
-    return (
-        <div className="Welcome">
-            Welcome Component
         </div>
     );
 }
