518c53f29c774b8e76ff20fded646b12a969792f
Jaeework <jaeework@gmail.com>
2024-03-27 23:12:22 +0900

    섹션12-4. React 에서 Spring Boot REST API 호출 확인
    axios 를 이용해 호출하고 값을 받아오는지 확인

--- full-stack/frontend/todo-app/src/components/todo/WelcomeComponent.jsx
+++ full-stack/frontend/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,8 +1,37 @@
 import { useParams, Link } from "react-router-dom";
+import axios from 'axios';
+import { useState } from 'react';
 
 function WelcomeComponent() {
 
     const { username } = useParams();
+
+    const [message, setMessage] = useState(null);
+
+    function callHelloWorldRestApi() {
+        console.log('called');
+        // axios
+        // axios.get('http://localhost:8080/hello-world')
+        // .then( (response) => successfulResponse(response) )
+        // .catch( (error) => errorResponse(error) )
+        // .finally ( () => console.log('cleanup') );
+
+        axios.get('http://localhost:8080/hello-world-bean')
+        .then( (response) => successfulResponse(response) )
+        .catch( (error) => errorResponse(error) )
+        .finally ( () => console.log('cleanup') );
+
+    }
+
+    function successfulResponse(response) {
+        console.log(response);
+        //setMessage(response.data);
+        setMessage(response.data.message);
+    }
+
+    function errorResponse(error) {
+        console.log(error);
+    }
 
     return (
         <div className="WelcomeComponent">
@@ -10,6 +39,10 @@ function WelcomeComponent() {
             <div>
                 Manage Your Todos. - <Link to='/todos'>Go here</Link>
             </div>
+            <div>
+                <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>Call Hello World</button>
+            </div>
+            <div className="text-info">{message}</div>
         </div>
     );
 }
