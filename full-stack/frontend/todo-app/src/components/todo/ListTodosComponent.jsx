02e6849173b5b30d62f54819934f6fd246fc8f55
Jaeework <jaeework@gmail.com>
2024-03-28 14:25:40 +0900

    섹션12-10. Todo 개별 항목 불러오고 삭제 하는 REST API 구현
    Todo 개별 항목을 불러오고 삭제하는 REST API 를 작성하고 React 프론트엔드 기초 코드 작성

--- full-stack/frontend/todo-app/src/components/todo/ListTodosComponent.jsx
+++ full-stack/frontend/todo-app/src/components/todo/ListTodosComponent.jsx
@@ -1,25 +1,47 @@
+import {useEffect, useState} from "react";
+import {deleteTodoApi, retrieveAllTodosForUsernameApi} from "./api/TodoApiService";
+
 export default function ListTodosComponent() {
 
     const today = new Date();
     const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay())
 
-    const todos =[
-        {id : 1, description : 'Learn AWS', done: false, targetDate: targetDate},
-        {id : 2, description : 'Learn Full Stack Dev', done: false, targetDate: targetDate},
-        {id : 3, description : 'Learn DevOps', done: false, targetDate: targetDate},
-        ]
+    const [todos, setTodos] = useState([]);
+    const [message, setMessage] = useState(null);
+
+    useEffect( () => refreshTodos, [] )
+
+    function refreshTodos() {
+        retrieveAllTodosForUsernameApi('in28minutes')
+            .then( response => {setTodos(response.data)} )
+            .catch( error => console.log(error) );
+    }
+
+    function deleteTodo(id) {
+        console.log('clicked ' + id);
+        deleteTodoApi('in28minutes', id)
+            .then(
+                () => {
+                    setMessage(`Delete of todo with id = ${id} successful`);
+                    refreshTodos();
+                }
+                //1. display message
+                //2. update todos list
+            ).catch(error => console.log(error))
+    }
 
     return (
         <div className="container">
             <h1>Things You Want To Do!</h1>
+            {message && <div className="alert alert-warning">{message}</div>}
             <div>
                 <table className="table">
                     <thead>
                         <tr>
-                            <td>ID</td>
-                            <td>Description</td>
-                            <td>Is Done?</td>
-                            <td>Target Date</td>
+                            <th>Description</th>
+                            <th>Is Done?</th>
+                            <th>Target Date</th>
+                            <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -27,10 +49,12 @@ export default function ListTodosComponent() {
                             todos.map(
                                 todo => (
                                     <tr key={todo.id}>
-                                        <td>{todo.id}</td>
                                         <td>{todo.description}</td>
                                         <td>{todo.done.toString()}</td>
-                                        <td>{todo.targetDate.toDateString()}</td>
+                                        {/*<td>{todo.targetDate.toDateString()}</td>*/}
+                                        <td>{todo.targetDate.toString()}</td>
+                                        <td><button className="btn btn-warning"
+                                                    onClick={() => deleteTodo(todo.id)}>Delete</button></td>
                                     </tr>
                                 )
                             )
