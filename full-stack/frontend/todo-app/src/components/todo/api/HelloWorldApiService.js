ecbaa365d13192a8229d45c01c2307ae69341f5e
Jaeework <jaeework@gmail.com>
2024-04-02 16:28:52 +0900

    섹션12-24. axios 인터셉터 설정
    모든 요청에 token 을 변수로 직접 넣어주는 것이 아니라 하나의 Api Client 를 만들어 모든 요청을 인터셉트하여 헤더에 token 을 넣어주도록 설정.
    - ApiClient.js 작성 후 baseUrl 설정
    - AuthContext 에 Axios 인터셉터 설정

--- full-stack/frontend/todo-app/src/components/todo/api/HelloWorldApiService.js
+++ full-stack/frontend/todo-app/src/components/todo/api/HelloWorldApiService.js
@@ -1,25 +1,21 @@
-import axios from 'axios';
+import { apiClient } from './ApiClient'
 
 // export function retrieveHelloWorldBean() {
 //  return axios.get('http://localhost:8080/hello-world-bean')
 // }
-
-const apiClient = axios.create(
-    {
-        baseURL : 'http://localhost:8080'
-    }
-)
 
 export const retrieveHelloWorldBean 
     = () => apiClient.get('/hello-world-bean')
 
 // Response to preflight request doesn't pass access control check => Authentication header
 export const retrieveHelloWorldPathVariable
-    = (username) => apiClient.get(`/hello-world/path-variable/${username}`, {
-        headers: {
-            Authorization: 'Basic aW4yOG1pbnV0ZXM6ZHVtbXk='
-        }
-    })
+    = (username, token) => apiClient.get(`/hello-world/path-variable/${username}`
+    // , {
+    //     headers: {
+    //         Authorization: token
+    //     }
+    // }
+    )
 
 export const executeBasicAuthenticationService
     = (token) => apiClient.get(`/basicauth`, {
